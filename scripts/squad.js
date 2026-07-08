ed2f08bb34012118fe36c477c113324e8d82d627
Peter Migichi <petermigichi47@gmail.com>
2026-07-08 10:03:21 +0300

    player name font will reduce when it surpasses 8 characters

--- scripts/squad.js
+++ scripts/squad.js
@@ -22,34 +22,30 @@ function renderStartingTeam() {
   const messages = [
     // 'You have been given a starting squad of 16 EPL players',
     // 'Goalkeepers',
-    // 'Bernd Leno, Dean Henderson',
     // 'Defenders',
-    // 'Ryan Giles, Leif Davis, Gabriel Gudmundsson, Milos Kerkez, Michael Keane',
     // 'Midfielders',
-    // `Martin Zubimendi, Manuel Ugarte, Luke O'Nien, Xavi Simons, Frank Onyeka`,
     // 'Forwards',
-    // 'Omar Marmoush, Morgan Rogers, Luis Sinisterra, Gustavo Nunes, Savinho',
   ];
 
   players.forEach((category) => {
 
     category.players.forEach((player) => 
       setTimeout(() => {
+        
+        const nameClass = player.name.length > 8
+        ? 'name-big' : '';
+
         document.querySelector('.js-squad').innerHTML = 
           `
             <div class="player">
               <img src="${player.image}" alt="" />
               <div class="player_details">
-                <div class="name">${player.name}</div>
+                <div class="name ${nameClass}">${player.name}</div>
                 <div class="xp">${player.rating}</div>
               </div>
             </div>
-          `
-        }, 1000 * player.id)
-      )
-
-    setTimeout(() => {
-      
-    }, 2000);
+          `;
+      }, 1000 * player.id) 
+    )
   })
 }
